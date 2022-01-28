89618e17a406c1736bec8e1d23eba28e8eb94458
unknown <lopez1432.al@gmail.com>
2022-01-27 16:43:06 -0800

    completed publci folder including css and js needed

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value;
+    const password = document.querySelector('#username-login').value;
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username, password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok){
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    };
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+const res = require("express/lib/response");
+
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application.json'
+        }
+    });
+
+    if (response.ok){
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
